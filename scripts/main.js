a7bcc4abd1a5dbc3867ca67a2bfa066be6da215b
JeyPey <tudorkawasaki@gmail.com>
2022-06-27 17:27:27 +0300

    gotovo
    gotovo

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// 3. Написать функцию, которая заменяет в полученной строке большие буквы на маленькие, маленькие – на большие, а цифры – на знак нижнего подчеркивания.
+
+let text = 'PIZZA hamburger HoTDoG 23324';
+
+function some(string) {
+    let result = "";
+
+    for (let i = 0; i < string.length; i++) {
+        const curSumbol = string[i];
+
+        if (Number(curSumbol)) {
+            result += "_";
+            continue;
+        }
+
+        if (curSumbol === curSumbol.toUpperCase()) {
+            result += curSumbol.toLowerCase();
+        } else {
+            result += curSumbol.toUpperCase();
+        }
+    }
+
+    return result;
+}
+
+console.log(some(text));
